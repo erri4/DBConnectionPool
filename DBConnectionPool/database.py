06d5e728ddbbca0ab4526af4321ad9803f2181de
rif av <reef.abramson@gmail.com>
2025-05-26 07:11:29 +0300

    add stub files

--- DBConnectionPool/database.py
+++ DBConnectionPool/database.py
@@ -3,85 +3,10 @@ import dbutils.pooled_db
 import pymysql.cursors
 import pymysql.connections
 from . import interfaces
-from typing import Callable, Any
-
-
-class _Column(str):
-    pass
-
-
-class _Row(dict[_Column, ]):
-    pass
-
-
-class Table:
-    """
-    a class for managing the table data.
-    """
-
-
-    def __init__(self, data: list[_Row], columns: list[str]) -> None:
-        """
-        store the data.
-
-        <code>data: list[_Row]:</code> the data of the table.<br>
-        <code>columns: list of strings:</code> the columns of the table.
-
-        <code>return: None. </code>
-        """
-
-        self.data = data
-        self.length = len(data)
-        self.columns = columns
-
-    
-    def get(self, row: int, column: str = None) -> dict[_Column, ] | Any | None:
-        """
-        get the data of the given column.
-
-        <code>column: string:</code> the column to get.
-
-        <code>return: list: </code> the data of the column.
-        """
-        return self.data[row][column] if column in self.data[row] else None if column else self.data[row] if row < len(self.data) else None
-
-
-class ReturnedSqlType:
-    """
-    a class for managing the returned data from the databse.
-    """
-
-
-    def __init__(self, sqlres: list[_Row], rowcount: int, close: Callable, columns: list[str]) -> None:
-        """
-        store the data.
-        
-        <code>sqlres: list of dictionarys:</code> the data itself.<br>
-        sqlres is build like this:
-        [row1, row2, ...]
-        each row is:
-        {column1: value, column2: value, ...}<br>
-        <code>rowcount: integer:</code> the rowcount.<br>
-        <code>close: callable:</code> a disconnect function.<br>
-        <code>columns: list of strings:</code> the columns of the table.<br>
-        
-        <code>return: None. </code>
-        """
-        self.sqlres = Table(sqlres, columns)
-        self.rowcount = rowcount
-        self.close = close
-
-
-    def __enter__(self):
-        return self
-
-
-    def __exit__(self, *exc) -> None:
-        self.close()
 
 
 class ConnectionPool(interfaces.ConnectionPoolInterface):
-    def __init__(self, password: str, user: str = 'root', host: str = 'localhost', port: int = 3306, database: str = None) -> None:
+    def __init__(self, password: str, user: str = 'root', host: str = 'localhost', port: int = 3306, database: str | None = None) -> None:
         """
         a class for managing the connection pool.
 
@@ -146,7 +71,7 @@ class ConnectionPool(interfaces.ConnectionPoolInterface):
         return r
 
 
-    def select(self, sql: str) -> ReturnedSqlType:
+    def select(self, sql: str) -> interfaces.ReturnedSqlType:
         """
         select data from the database.
 
@@ -154,11 +79,10 @@ class ConnectionPool(interfaces.ConnectionPoolInterface):
 
         <code>return: _ReturnedSql: </code> an instance of the _ReturnedSql class containing the rowcount, the data itself, and a disconnect function.
         """
-        result = []
         conn = self._connect()
-        with conn.cursor() as cursor:
-            cursor: pymysql.cursors.DictCursor
-            cursor.execute(sql)
-            columns =[desc[0] for desc in cursor.description] if cursor.description else []
-            result = ReturnedSqlType(cursor.fetchall(), cursor.rowcount, lambda: self._disconnect(conn), columns)
+        with conn.cursor() as crsor:
+            crsor: pymysql.cursors.DictCursor
+            crsor.execute(sql)
+            columns =[desc[0] for desc in crsor.description] if crsor.description else []
+            result = interfaces.ReturnedSqlType(crsor.fetchall(), crsor.rowcount, lambda: self._disconnect(conn), columns)
             return result
